e523defbae2a07d9be6f1d858b07511198d79fc4
Brock Smith <brocksmith225@gatech.edu>
2019-09-17 17:41:21 -0400

    Added method for each of the welcome, configuration, and configuration display screens; Added action listener to move from the welcome screen to the configuration screen when the start button is pressed

--- src/team78/spacetrader/ui/SpaceTrader.java
+++ src/team78/spacetrader/ui/SpaceTrader.java
@@ -2,9 +2,28 @@ package team78.spacetrader.ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+/**
+ * Provides options for difficulties
+ */
+enum Difficulty {
+    EASY, MEDIUM, HARD;
+}
+
+/**
+ * This class handles the GUI for the Space Trader game.
+ */
 public class SpaceTrader {
+
     private JFrame frame;
+
+    // Configuration values
+    private String name;
+    private Difficulty difficulty;
+    private int[] skillPoints;
+    private int credits;
 
     /**
      * Creates a new Space Trader game by initializing the GUI with the welcome screen
@@ -19,8 +38,11 @@ public class SpaceTrader {
         // Sets the content to the welcome screen
         frame.getContentPane().add(createWelcomePanel());
 
-        // Sets the size of the frame to the smallest that can fit the content
-        frame.pack();
+        // Sets the frame to windowed fullscreen
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        // Sets the frame to fullscreen
+        //frame.setUndecorated(true);
 
         // Shows the frame
         frame.setVisible(true);
@@ -35,14 +57,58 @@ public class SpaceTrader {
         // Panel that contains all of the content for the welcome screen
         JPanel welcomePanel = new JPanel();
 
-        // Adds the welcome header
+        // Creates and adds the welcome header to the panel
         welcomePanel.add(createHeader("Welcome to Space Trader!"));
 
-        // Adds the start button
+        // Creates the start button
         JButton startButton = new JButton("START");
+
+        // Adds a listener to start the game when the button is pressed
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Removes the content from the welcome screen
+                frame.getContentPane().removeAll();
+                frame.repaint();
+
+                // Sets the content to the configuration screen
+                frame.getContentPane().add(createConfigurationPanel());
+            }
+        });
+
+        // Adds the start button to the panel
         welcomePanel.add(startButton);
 
         return welcomePanel;
+    }
+
+    /**
+     * Creates the initial configuration panel
+     *
+     * @return the initial configuration panel
+     */
+    private JPanel createConfigurationPanel() {
+        //TODO Create configuration panel where the user can configure the following options:
+        // - Name
+        // - Difficulty
+        // - Skill Points
+
+        return null;
+    }
+
+    /**
+     * Creates the final panel that displays the player's configuration
+     *
+     * @return the configuration display panel
+     */
+    private JPanel createConfigurationDisplayPanel() {
+        //TODO Create panel that displays the player's configuration from the previous frame:
+        // - Name
+        // - Difficulty
+        // - Skill Points
+        // - Credits (calculated based on the difficulty setting)
+
+        return null;
     }
 
     /**
