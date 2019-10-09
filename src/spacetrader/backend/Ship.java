8b16453d0a533b547a6dc673c4fe6f5512b4d3f8
ChrisGoKim <chrisjokim@gmail.com>
2019-10-09 14:35:38 -0400

    Added improved constructor for Ship along with the Enum. Added methods to alter the Ship's variables

--- src/spacetrader/backend/Ship.java
+++ src/spacetrader/backend/Ship.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class Ship {
     private String type;
     private int currentSpace;
-    private int maxCargoSpace = 100;
+    private int maxCargoSpace;
     private int currentFuel;
-    private int maxFuelCapacity = 500;
-    private int health = 100;
+    private int maxFuelCapacity;
+    private int currentHealth;
+    private int maxHealth;
 
     private String[] typeList = {
             "Starship",
@@ -18,13 +19,116 @@ public class Ship {
     };
 
     /**
-     *
+     * Constructor for Ship that sets the type along with the variable changes
+     * that the type entails
+     * @param typeEnum ShipType enum for the switch statement
      */
-    public Ship() {
-        Random ran = new Random();
-        type = typeList[ran.nextInt(3)];
+    public Ship(ShipType typeEnum) {
+        switch (typeEnum) {
+            case STARSHIP:
+                maxCargoSpace = 500;
+                maxFuelCapacity = 500;
+                maxHealth = 500;
+                break;
+            case JET:
+                maxCargoSpace = 100;
+                maxFuelCapacity = 100;
+                maxHealth = 100;
+                break;
+            case WASP:
+                maxCargoSpace = 100;
+                maxFuelCapacity = 200;
+                maxHealth = 50;
+                break;
+            case LADYBUG:
+                maxCargoSpace = 200;
+                maxFuelCapacity = 50;
+                maxHealth = 150;
+                break;
+        }
         currentSpace = 0;
         currentSpace = 0;
     }
 
+    /**
+     * Current fuel getter function
+     * @return current fuel as an int
+     */
+    public int getCurrentFuel() {
+        return currentFuel;
+    }
+
+    /**
+     * Current health getter function
+     * @return current health as an int
+     */
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    /**
+     * Current space getter function
+     * @return current sapce as an int
+     */
+    public int getCurrentSpace() {
+        return currentSpace;
+    }
+
+    /**
+     * Function to alter the fuel; can add or subtract
+     * @param variable amount that the fuel should be affected
+     * @return boolean based on whether successfully fueled up or not
+     * can be used as a check to determine whether the player can travel
+     * given their current fuel supply
+     */
+    public boolean alterCurrentFuel(int variable) {
+        if (currentFuel + variable > maxFuelCapacity) {
+            currentFuel = maxFuelCapacity;
+            return true;
+        } else if (currentFuel + variable < 0) {
+            return false;
+        } else {
+            currentFuel += variable;
+            return true;
+        }
+    }
+
+    /**
+     * Function to alter the health; can add or subtract
+     * @param variable amount that the health should be affected
+     * @return boolean based on whether successfully added health or not
+     * can be used as a check to determine whether the player has died
+     * or made it max health
+     */
+    public boolean alterCurrentHealth(int variable) {
+        if (currentHealth + variable > maxHealth) {
+            currentHealth = maxHealth;
+            return true;
+        } else if (currentHealth + variable < 0) {
+            return false;
+        } else {
+            currentHealth += variable;
+            return true;
+        }
+    }
+
+    /**
+     * Function to alter the space; can add or subtract
+     * @param variable amount that the space should be affected
+     * @return boolean based on whether successfully affected the amount of
+     * space that is left or not can be used as a check to determine whether
+     * the player cannot add any more items or there's max space available
+     */
+    public boolean alterCurrentSpace(int variable) {
+        if (currentSpace + variable > maxFuelCapacity) {
+            currentSpace = maxFuelCapacity;
+            return true;
+        } else if (currentSpace + variable < 0) {
+            currentSpace = 0;
+            return false;
+        } else {
+            currentSpace += variable;
+            return true;
+        }
+    }
 }
---
+++ src/spacetrader/backend/ShipType.java
@@ -0,0 +1,5 @@
+package spacetrader.backend;
+
+public enum ShipType {
+    STARSHIP, JET, WASP, LADYBUG
+}
